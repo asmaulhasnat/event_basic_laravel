800d048b4ae65187a380b77bc6878f21ce9c6c67
asmaul_hasnat <ahsweet92@gmail.com>
2018-10-23 23:32:15 +0600

    basic event example

---
+++ app/Events/OrderShipped.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class OrderShipped {
+	use Dispatchable, InteractsWithSockets, SerializesModels;
+
+	/**
+	 * Create a new event instance.
+	 *
+	 * @return void
+	 */
+	public $sum;
+	public function __construct($sum) {
+		$this->sum = $sum;
+	}
+
+	/**
+	 * Get the channels the event should broadcast on.
+	 *
+	 * @return \Illuminate\Broadcasting\Channel|array
+	 */
+	public function broadcastOn() {
+		return new PrivateChannel('channel-name');
+	}
+}
